c8a0f5c3a3d563a835623815d66363cec83349bf
Cam Millsap <cam.millsap12@gmail.com>
2021-11-02 12:49:14 -0400

    Chapter 10 - Robots spawn on teleporters

---
+++ Assets/Scripts/PickupSpawn.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] pickups;
+
+    void spawnPickup()
+    {
+        //Instantiate a random pickup
+        GameObject pickup = Instantiate(pickups[Random.Range(0, pickups.Length)]);
+        pickup.transform.position = transform.position;
+        pickup.transform.parent = transform;
+    }
+
+    IEnumerator respawnPickup()
+    {
+        yield return new WaitForSeconds(20);
+        spawnPickup();
+    }
+
+    private void Start()
+    {
+        spawnPickup();
+    }
+
+    public void PickupWasPIckedUp()
+    {
+        StartCoroutine("respawnPickup");
+    }
+}
